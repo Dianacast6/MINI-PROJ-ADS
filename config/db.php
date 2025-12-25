4afdeed69c024f9541140f18c31f266707ad8100
janusinss <janusdominic0@gmail.com>
2025-12-25 20:04:55 +0800

    FINISHED1

--- config/db.php
+++ config/db.php
@@ -1,8 +1,22 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "quicknote_db"; // UPDATED HERE
+// DATABASE CONFIGURATION
+// DATABASE CONFIGURATION
+
+$whitelist = array('127.0.0.1', '::1', 'localhost');
+
+if (in_array($_SERVER['REMOTE_ADDR'], $whitelist) || $_SERVER['SERVER_NAME'] == 'localhost') {
+    // Localhost (XAMPP)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "quicknote_db";
+} else {
+    // InfinityFree Deployment
+    $servername = "sql305.infinityfree.com";
+    $username = "if0_40760361";
+    $password = "v8DVyYWMwrkFJ6A";
+    $dbname = "if0_40760361_quicknote_db";
+}
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
